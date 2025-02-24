498eb5bf85714d3e53d61d728fcfb6feb5fd9bbe
Greg Hogue <greg.hogue@gmail.com>
2025-02-24 15:16:53 -0500

    Updates

--- static/js/952.ba4d7873.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkworkshop_app=self.webpackChunkworkshop_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.ba4d7873.chunk.js.map
